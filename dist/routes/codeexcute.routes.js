47e943f7f163b0439137878f13570bb0bd202482
9348abinash-nayak <nayakabiansh746@gmail.com>
2026-04-17 20:01:02 +0530

    backend

--- dist/routes/codeexcute.routes.js
+++
@@ -1,4 +0,0 @@
-import express from 'express';
-import { executeCodeController } from "../controllers/excute.room.controller.js";
-export const codeexecuteRouter = express.Router();
-codeexecuteRouter.post("/run", executeCodeController);
--- dist/routes/room.routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { createRoomController, getRoomController, joinRoomController, lockRoomController } from "../controllers/room.controller.js";
-export const roomRouter = express.Router();
-roomRouter.post("/create", createRoomController);
-roomRouter.post("/join", joinRoomController);
-roomRouter.get("/get/:roomId", getRoomController);
-roomRouter.post("/locked", lockRoomController);
